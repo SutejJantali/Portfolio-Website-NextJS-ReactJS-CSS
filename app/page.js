bef8f868e1e15a1bcc7b780e2eb230c31ecc8b7a
Sutej <iamsutej@gmail.com>
2025-07-11 01:59:56 +0530

    Fix image display issue and update build

--- app/page.js
+++ app/page.js
@@ -79,7 +79,7 @@ export default function Home() {
             </div>
           </div>
           <div className="hero-yellow">
-            <img src="/imgs/dp_new.png" alt="Sutej Jantali" style={{ width: '100%', maxWidth: '600px', objectFit: 'contain' }} />
+            <img src="/imgs/dp_new.PNG" alt="Sutej Jantali" style={{ width: '100%', maxWidth: '600px', objectFit: 'contain' }} />
           </div>
         </section>
         <section className="logos container">
